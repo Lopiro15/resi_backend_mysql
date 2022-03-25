12133d9f162f81620c174be1032cba5f8e5b6007
Free Dev Group Git <freedev6@gmail.com>
2022-03-25 16:19:24 +0000

    ajout des fichiers nécessaires pour le mode production

--- Resi_mysql/settings.py
+++ Resi_mysql/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME' : 'resi_db',
         'USER' : 'root',
         'PASSWORD' : 'Freedev2020@mysql',
-        'HOST' : 'localhost',
+        'HOST' : 'db',
     }
 }
 
--- Resi_mysql/wsgi.py
+++ Resi_mysql/wsgi.py
@@ -12,9 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #For developpment mode
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resi_mysql.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resi_mysql.settings')
 
 #For production mode
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resi_mysql.production')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resi_mysql.production')
 
 application = get_wsgi_application()
